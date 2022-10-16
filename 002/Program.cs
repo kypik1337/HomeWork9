14b4f54a47cef3c795642070003b862893ca48e8
tema <kypidon71@gmail.com>
2022-10-16 17:54:32 +0300

    002

---
+++ 002/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.WriteLine("Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N");
+Console.WriteLine("Add number M");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Add number N");
+int number2 = int.Parse(Console.ReadLine()!);
+
+int ReSumm(int number, int number2)
+		{
+			int GoStart = number;
+			int Finish = number2;
+			if(number > number2)
+			{
+				GoStart = number2;
+				Finish = number;
+			}
+			return (Finish + GoStart)*(Finish - GoStart + 1)/2;
+	}
+Console.WriteLine(ReSumm(number,number2));
